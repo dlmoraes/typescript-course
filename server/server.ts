f4b2a620d1000238508b0b9060720e57c2fed2c1
Diego Moraes <diegofrost96@gmail.com>
2019-01-14 11:37:20 -0300

    Modelando Dados com Sequelize e Sincronizando com o Banco de Dados

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,15 @@
 import * as http from 'http';
 import Api from './api/api'
 
+const models = require('./models')
+
 const config = require('./config/env/config')()
 
 const server = http.createServer(Api);
 
-server.listen(config.serverPort)
+models.sequelize.sync().then(() => {
+    server.listen(config.serverPort)
 
-server.on('listening', () => console.log(`Servidor rodando na porta ${config.serverPort}`))
-server.on('error', (error: NodeJS.ErrnoException) => console.log(`Ocorreu um erro: ${error}`))
+    server.on('listening', () => console.log(`Servidor rodando na porta ${config.serverPort}`))
+    server.on('error', (error: NodeJS.ErrnoException) => console.log(`Ocorreu um erro: ${error}`))
+})
