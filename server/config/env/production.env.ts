e01e20abc4a4c89086b65ddd289b1636cf015345
Diego Moraes <diegofrost96@gmail.com>
2019-01-23 23:12:34 -0300

    Estabilizando aplicação no ambiente de desenvolvimento

---
+++ server/config/env/production.env.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    env: 'production',
+    db: 'ts-api',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: '1390',
+    host: 'localhost',
+    serverPort: '3000',
+    pgPort: 5432,
+    dbURL: 'postgres://postgres:1390@localhost:5432/ts-api',
+    secret: 'S3cr3t'
+}
